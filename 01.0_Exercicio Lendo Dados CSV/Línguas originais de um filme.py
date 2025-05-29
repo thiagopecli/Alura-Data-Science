1ba1097fef318af5e2d2211580b2e6ecbd8f86d2
thiagopecli <thiagopecli.jjboy@gmail.com>
2025-05-29 02:50:41 -0300

    nomes e indices

--- 01.0_Exercicio Lendo Dados CSV/Línguas originais de um filme.py
+++ 01.0_Exercicio Lendo Dados CSV/Línguas originais de um filme.py
@@ -12,3 +12,25 @@ print(tmdb['original_language'].unique())
 # Exibir qual moda da coluna 'original_language':
 print(tmdb['original_language'].value_counts())
 
+# Exibir os indices dos filmes:
+print(tmdb['original_language'].value_counts().index)
+
+# Exibir os valores da moda:
+print(tmdb['original_language'].value_counts().values)
+
+# Transformar em dataframe:
+print(tmdb['original_language'].value_counts().to_frame())
+
+# Correção de nomes e índices:
+print(tmdb['original_language'].value_counts().to_frame().reset_index())
+
+# Renomear as Colunas:
+print(tmdb['original_language'].value_counts().to_frame().reset_index().rename(columns={'original_language': 'Idioma', 'count': 'Quantidade'}))
+
+# Visualizar no gráfico:
+import matplotlib.pyplot as plt
+tmdb['original_language'].value_counts().to_frame().reset_index().rename(columns={'original_language': 'Idioma', 'count': 'Quantidade'}).plot(x='Idioma', y='Quantidade', kind='bar')
+plt.title('Línguas Originais dos Filmes')
+plt.xlabel('Idioma')
+plt.ylabel('Quantidade')
+plt.show()
