af271ebb3e590f9af459229c38d3da635fbf2bf5
thiagopecli <thiagopecli.jjboy@gmail.com>
2025-06-05 22:57:48 -0300

    revisão

---
+++ 01.0_Exercicio Lendo Dados CSV/reforço analisando distribuição boxplot.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Carregando os dados e exibindo ass primeiras 3 linhas:
+filmes = pd.read_csv(r'e:\Documentos\FACULDADE\Alura---Data-Science\01.0_Exercicio Lendo Dados CSV\movies.csv')
+print(filmes.head(3))
+
+notas = pd.read_csv(r'e:\Documentos\FACULDADE\Alura---Data-Science\01.0_Exercicio Lendo Dados CSV\ratings.csv')
+print(notas.head(3))
+
+# Extraindo as notas dos dois filmes em variáveis distintas:
+notas_do_toy_story = notas.query('movieId == 1')
+notas_do_jumanji = notas.query('movieId == 2')
+
+media_do_toy_story = notas_do_toy_story["rating"].mean()
+media_do_jumanji = notas_do_jumanji["rating"].mean()
+
+print(media_do_toy_story, media_do_jumanji)
