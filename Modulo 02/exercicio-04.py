9bc07bcabca04a403baa0c8cb0c1965a67f36f72
thiagopecli <thiagopecli.jjboy@gmail.com>
2025-08-25 20:10:39 -0300

    exponenciação, modulo e divisão inteira

---
+++ Modulo 02/exercicio-04.py
@@ -0,0 +1,28 @@
+# Calculos com Exponenciação, módulo e divissão inteira:
+
+# Exponenciação(**):
+num = 2**3
+print(num)
+
+# OU ------------
+operador = 5
+potencia = 3
+print(operador ** potencia)
+
+# Módulo(%): Resto da divisão
+num = 7 % 3
+print(num)
+
+# OU ------------
+dividendo = 11
+divisor = 3
+print(dividendo % divisor)
+
+# Divisão inteira(//): Retorna o resultado da divisão inteira entre dois numeros
+num = 10 // 3
+print(num)
+
+# OU ------------
+numerador = 10
+denominador = 2
+print(numerador // denominador)
