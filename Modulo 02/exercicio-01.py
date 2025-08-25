9371bd26c602013f254e1c87fd737c40b1f3692d
thiagopecli <thiagopecli.jjboy@gmail.com>
2025-08-25 16:19:26 -0300

    exercicio 01

---
+++ Modulo 02/exercicio-01.py
@@ -0,0 +1,36 @@
+# Exercicio 01:
+
+print("Escola de Dados Alura!")
+
+nome = "[Thiago]"
+sobrenome = "[Pereira]"
+print(nome, sobrenome)
+
+nome1 = "Thiago"
+print("\n".join(nome1))
+
+dia = 12
+mes = "Junho"
+ano = 1996
+print(dia, mes, ano)
+
+curso = 2025
+print(f"Ano atual: [{curso}]")
+
+# Gabarito Alura
+
+print("\nEscola de Dados Alura!")
+
+print("Nome: [Thiago]")
+print("Sobrenome: [Pereira]")
+
+print("T")
+print("H")
+print("I")
+print("A")
+print("G")
+print("O")
+
+print(12, "Junho", 1996)
+
+print("Ano atual:", 2025)
