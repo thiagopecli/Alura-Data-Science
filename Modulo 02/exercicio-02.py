d714a6ceec57deb10c03c69f953f7deeb460d980
thiagopecli <thiagopecli.jjboy@gmail.com>
2025-08-25 18:44:22 -0300

    exercicio 02

---
+++ Modulo 02/exercicio-02.py
@@ -0,0 +1,24 @@
+"""
+Podemos usar ** para colocar o texto em negrito
+Podemos usar * para colocar o texto em Itálico
+Podemos usar aspas simples ou dupla 3 vezes para comentar um paragrafo/texto
+Podemos usar '#' para comentar uma linha ou cabeçalho
+
+"""
+
+# ** Texto em negrito **
+# *Texto em itálico*
+# ***Texto em Negrito e Itálico***
+# '1.' Para Linhas enumeradas
+# Para construir links, usamos a seguinte sintaxe: [Texto do link](link) 
+# Atalho para alterar uma palavra multipla altomaticamente: "Ctrl + F2".
+
+nome_aluno = "Carlos" # Variável com tipo String
+idade_aluno = 29 # Variável com tipo inteiro
+nota_aluno = 9.1 # Variável com tipo float
+situacao = True # Variável com tipo boleano
+
+print(f"Valor: {nome_aluno}, Tipo: {type(nome_aluno)};") # Imprimindo na tela o tipo da variável
+print(f"Valor: {idade_aluno}, Tipo: {type(idade_aluno)};") # Imprimindo na tela o tipo da variável
+print(f"Valor: {nota_aluno}, Tipo: {type(nota_aluno)};") # Imprimindo na tela o tipo da variável
+print(f"Valor: {situacao}, Tipo: {type(situacao)}.") # Imprimindo na tela o tipo da variável
